a82987ed4732ec450d8a983de7cf4755af729f59
MominaGull <32342346+MominaGull@users.noreply.github.com>
2020-03-21 17:41:51 +0500

    Directory Commit

--- src/app.js
+++ src/app.js
@@ -7,10 +7,12 @@ const forecast = require('./utils/forecast')
 const app = express() 
 
 const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 app.set('view engine', 'hbs')
-app.set('views', './templates/views')
-hbs.registerPartials('./templates/partials')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
  
 app.use(express.static(publicDirectoryPath))
 
